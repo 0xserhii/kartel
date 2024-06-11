be65fd6fb59b8a7bfaa42fa2f4a31796fa793804
scottadms <terrykan0x0@gmail.com>
2024-06-12 01:47:13 +0800

    feat:other section

--- src/pages/games/slots/index.tsx
+++ src/pages/games/slots/index.tsx
@@ -1,7 +1,6 @@
 export default function SlotsGames() {
   return (
     <div className='flex flex-col gap-5 mt-3'>
-      <h1 className="text-white text-start text-3xl">Coming Soon...</h1>
     </div>
   );
 }
--- src/pages/help-support/index.tsx
+++ src/pages/help-support/index.tsx
@@ -1,7 +1,6 @@
 export default function HelpSupport() {
   return (
     <div className="mt-3">
-      <h1 className="text-white text-start text-3xl">Coming Soon...</h1>
     </div>
   );
 }
