e74e4e0da17eecbf997f80676861d63236bad8ec
caraX0 <iBranch211@outlook.com>
2024-06-11 12:51:27 -0700

    feat: lint issue

--- src/utils/socket-service.ts
+++ src/utils/socket-service.ts
@@ -2,7 +2,7 @@ import { IChatClientToServerEvents, IChatServerToClientEvents } from "@/types";
 import { ICoinflipClientToServerEvents, ICoinflipServerToClientEvents } from "@/types/coinflip";
 import { ILeaderboardClientToServerEvents, ILeaderboardServerToClientEvents } from "@/types/leader";
 import { Socket, io } from "socket.io-client";
-import customParser from 'socket.io-msgpack-parser'
+// import customParser from 'socket.io-msgpack-parser'
 
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
