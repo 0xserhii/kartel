241021cdd238417530c08091f091ab4a4c478b61
noahwhitee <terrykan0x0@gmail.com>
2024-06-12 09:44:42 +0800

    change logo

--- src/components/shared/mobile-sidebar.tsx
+++ src/components/shared/mobile-sidebar.tsx
@@ -3,7 +3,7 @@ import { Sheet, SheetContent } from '@/components/ui/sheet';
 import { navItems } from '@/constants/data';
 import { Dispatch, SetStateAction } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '/assets/logo.svg';
+import Logo from '/assets/logo.png';
 
 type TMobileSidebarProps = {
   className?: string;
@@ -20,7 +20,7 @@ export default function MobileSidebar({
         <SheetContent side="left" className="border-none bg-dark !px-0">
           <div className="flex w-full flex-col items-center gap-8 p-5 ">
             <Link to="/" className="py-2 text-2xl font-bold text-white ">
-              <img src={Logo} alt="Logo" />
+              <img src={Logo} alt="Logo" className="h-32 w-36" />
             </Link>
             <div className="w-full space-y-1 px-2">
               <DashboardNav items={navItems} setOpen={setSidebarOpen} />
