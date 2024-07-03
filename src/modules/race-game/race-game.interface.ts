443e1418159a711724b120d7c0600889d2d27e71
caraX0 <iBranch211@outlook.com>
2024-07-03 05:37:18 +0200

    fix: crash maximize

--- src/modules/race-game/race-game.interface.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Document } from "mongoose";
-
-export interface IRaceGameModel extends Document {
-  active: boolean;
-  prize: number;
-  endingDate: Date;
-  winners: mongoose.Types.ObjectId[];
-  created: Date;
-}
--- src/modules/race-game/swagger/index.ts
+++
@@ -1,2 +0,0 @@
-export * as exampleSchema from "./race-game.schema";
-export { default as exampleSwagger } from "./race-game.swagger";
