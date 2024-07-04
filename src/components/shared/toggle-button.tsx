4492838ac1c0967fef68f30d652ae151161c865b
angelecho <terrykan0x0@gmail.com>
2024-07-04 11:16:37 -0400

    fix: refresh betdata

--- src/components/shared/toggle-button.tsx
+++ src/components/shared/toggle-button.tsx
@@ -20,7 +20,7 @@ export default function ToggleButton() {
         <>
             <div className="flex items-center dark:text-white">
                 <div className="relative flex items-center mx-2 shadow-sm rounded-full gap-2 border-2 border-[#4a278da1] bg-transparent hover:bg-transparent">
-                    <div className="w-16 h-7 bg-transparent rounded-full z-20 flex flex-row justify-between px-1 items-center">
+                    <div className="px-2.5 h-7 bg-transparent rounded-full z-20 flex flex-row justify-between items-center gap-3.5">
                         <Music className={`z-10 w-5 cursor-pointer ${settings.isMusicPlay ? 'text-purple' : "text-white"}`} onClick={handleMusic} />
                         <Volume2 className={`z-10 w-5 cursor-pointer ${settings.isSoundPlay ? 'text-purple' : "text-white"}`} onClick={handleSound} />
                     </div>
