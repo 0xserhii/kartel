77d4ad220d61237f224affbe28eae594c217a47c
angelecho <terrykan0x0@gmail.com>
2024-07-04 01:51:41 -0400

    fix: crashpoint limit

--- src/utils/crypto/random.ts
+++ src/utils/crypto/random.ts
@@ -126,7 +126,10 @@ const generateCrashPoint = (seed: string, salt: string): number => {
   const h = parseInt(hash.slice(0, 52 / 4), 16);
   const e = Math.pow(2, 52);
 
-  return Math.floor((100 * e - h) / (e - h));
+  const crashPoint = Math.floor((100 * e - h) / (e - h));
+
+  // Ensure crash point is less than 1000
+  return Math.min(crashPoint, 999);
 };
 
 const isCrashHashDivisible = (hash: string, mod: number): boolean => {
